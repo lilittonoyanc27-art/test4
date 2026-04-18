086a4407af05bada4d0e8f0f67419cc093e6bbd4
lilittonoyanc27-art <lilit.tonoyanc27@gmail.com>
2026-04-18 09:47:53 +0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PronunciationQuest from './PronunciationQuest';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-950">
+      <PronunciationQuest />
+    </div>
+  );
+}
